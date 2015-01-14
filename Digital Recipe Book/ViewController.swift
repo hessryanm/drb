bbe7f5ad7b26f89995e4961b2abec163acbfb663
Ryan Hess <hess.ryanm@gmail.com>
2015-01-14 13:11:02 -0700

    initial commit

---
+++ Digital Recipe Book/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Digital Recipe Book
+//
+//  Created by Ryan Hess on 1/14/15.
+//  Copyright (c) 2015 Ryan Hess. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
